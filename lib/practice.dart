084ccaec017ee10f200c4115fa95226612860587
Awdhesh <awdheshdubey64@gmail.com>
2023-10-04 13:46:23 +0530

    Code by Awdhesh

--- lib/practice.dart
+++ lib/practice.dart
@@ -22,13 +22,30 @@ void main () {
 
 
 }
-*/
+
+
+// This Function Take the input then print the result
 
 void main (){
 
  stdout.write("Enter your Favorite Number: ");
- final  a = stdin.readLineSync();
+ var a = stdin.readLineSync();
 print('your Favorite Number is: $a');
 
 }
 
+
+// This function take the two integers as input and add the both number & give result
+void main (){
+
+ print("Enter First Number: ");
+ int? number1 = int.parse(stdin.readLineSync()!);
+
+ print("Enter Second Number: ");
+ int? number2 = int.parse(stdin.readLineSync()!);
+
+ int sum = number1 + number2;
+ print("Your Result is this: $sum");
+
+}
+*/
